199e931c299012c66d5b00c5cbf6d1a35d6a6625
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-30 16:43:22 +0100

    Created header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<div class="container d-flex justify-content-between align-items-center">
+    <div class="logo mx-3">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+    </div>
+    <nav>
+        <ul class="d-flex">
+            <li class="mx-2"><a href="">CHARACTERS</a></li>
+            <li class="selected mx-2"><a href="">COMICS</a></li>
+            <li class="mx-2"><a href="">MOVIES</a></li>
+            <li class="mx-2"><a href="">TV</a></li>
+            <li class="mx-2"><a href="">GAMES</a></li>
+            <li class="mx-2"><a href="">COLLECTIBLES</a></li>
+            <li class="mx-2"><a href="">VIDEOS</a></li>
+            <li class="mx-2"><a href="">FANS</a></li>
+            <li class="mx-2"><a href="">NEWS</a></li>
+            <li class="mx-2"><a href="">SHOP</a></li>
+        </ul>
+    </nav>
+    <input class="click mb-3 text-end" type="text" placeholder="Search">
+</div>
