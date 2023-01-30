7c9ea0682ae8a93a5e0f269d4fc511701abcead5
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-30 17:47:44 +0100

    created footer and update in different files

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,58 @@
+<div class="container-footer">
+    <section class="footer-up">
+        <div class="lists">
+            <ul>
+                <h3>DC COMICS</h3>
+                    <li class="mx-2"><a href="">Characters</a></li>
+                    <li class="selected mx-2"><a href="">Comics</a></li>
+                    <li class="mx-2"><a href="">Movies</a></li>
+                    <li class="mx-2"><a href="">Tv</a></li>
+                    <li class="mx-2"><a href="">Games</a></li>
+                    <li class="mx-2"><a href="">Videos</a></li>
+                    <li class="mx-2"><a href="">News</a></li>
+                <div>
+                    <ul class="special-list">
+                        <h3>SHOP</h3>
+                            <li class="mx-2"><a href="">Shop DC</a></li>
+                            <li class="selected mx-2"><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+            </ul>
+            <ul>
+                <h3>DC</h3>
+                    <li class="mx-2"><a href="">Terms of Use</a></li>
+                    <li class="selected mx-2"><a href="">Privacy Policy</a></li>
+                    <li class="mx-2"><a href=""></a>Ad Choiches</li>
+                    <li class="mx-2"><a href=""></a>Advertising</li>
+                    <li class="mx-2"><a href="">Jobs</a></li>
+                    <li class="mx-2"><a href="">Subscriptions</a></li>
+                    <li class="mx-2"><a href="">Talent Workshops</a></li>
+                    <li class="mx-2"><a href="">CSPC Certificates</a></li>
+                    <li class="selected mx-2"><a href="">Rating</a></li>
+                    <li class="mx-2"><a href=""></a>Shop Help</li>
+                    <li class="mx-2"><a href=""></a>Contact Us</li>
+            </ul>
+            <ul>
+                <h3>SITES</h3>
+                <li class="mx-2"><a href="">DC</a></li>
+                    <li class="mx-2"><a href="">MAD Magazine</a></li>
+                    <li class="selected mx-2"><a href="">DC Kids</a></li>
+                    <li class="mx-2"><a href=""></a>Dc Universe</li>
+                    <li class="mx-2"><a href=""></a>Dc Power Visa</li>
+            </ul>
+        </div>
+        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="logo">
+    </section>
+
+    <section class="footer-down">
+        <button>SIGN-UP NOW!</button>
+            <h3><strong>FOLLOW US</strong></h3>
+            <ul>
+                <li><a href=""><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook-logo"></a></li>
+                <li><a href=""><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope-logo"></a></li>
+                <li><a href=""><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest-logo"></a></li>
+                <li><a href=""><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter-logo"></a></li>
+                <li><a href=""><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube-logo"></a></li>
+            </ul>
+    </section>
+</div>
