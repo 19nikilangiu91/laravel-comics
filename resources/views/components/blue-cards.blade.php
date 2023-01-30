3865f9950e8c9b5c00233746942d6e7ee6a7c208
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-30 20:41:51 +0100

    Update blue-cards

---
+++ resources/views/components/blue-cards.blade.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+    <div class="cards d-flex justify-content-center py-2">
+        <div class="blue-card d-flex">
+            <div class="image">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="logo">
+            </div>
+            <div class="text p-4">
+                <span>DIGITAL COMICS</span>
+            </div>
+        </div>
+        <div class="blue-card d-flex">
+            <div class="image">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="logo">
+            </div>
+            <div class="text p-4"">
+                <span>DC MERCHANDISE</span>
+            </div>
+        </div>
+        <div class="blue-card d-flex">
+            <div class="image">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="logo">
+            </div>
+            <div class="text p-4"">
+                <span>SUBSCRIPTION</span>
+            </div>
+        </div>
+        <div class="blue-card d-flex">
+            <div class="image">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="logo">
+            </div>
+            <div class="text p-4"">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+        </div>
+        <div class="blue-card d-flex">
+            <div class="special-image">
+                <img src="" alt="">
+            </div>
+            <div class="text p-4"">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+</div>
