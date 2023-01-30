4f81431283135bdc3e552cb3d5d22e04a56208de
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-30 16:41:56 +0100

    Update pages and layout

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel - Comics</title>
+    @Vite('resources/js/app.js')
     @yield('head')
 </head>
 <body>
+    @include('components.header')
     @yield('content')
 </body>
 </html>
--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-<h1>Comics Current Series</h1>
+
     
 @endsection
